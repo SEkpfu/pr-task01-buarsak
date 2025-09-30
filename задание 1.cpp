09a8a44822997db7a1c6ed75b8632ce3d6cde4e6
buarsak <yanovyan151@gmail.com>
2025-09-30 22:16:56 +0300

    Add files via upload

---
+++ задание 1.cpp
@@ -0,0 +1,11 @@
+// задание 1
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	double a, b;
+	cin >> a;
+	cin >> b;
+	cout << "Общая заработная плата=" << a * b << endl;
+	return 0;
+}
